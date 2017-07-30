b224a385c52043f37ab07db9ff8147877d8e4482
Rico Kahler <ricokahler@me.com>
2017-07-30 04:38:33 -0400

    questions api

--- index.js
+++ index.js
@@ -1,18 +1,86 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+const uuid = require('uuid/v4');
+const bodyParser = require('body-parser');
 
 app.set('port', (process.env.PORT || 5000));
-
+app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
 
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+let questions = [];
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
+/**
+ * Validates a question object
+ * @param {string} question 
+ */
+function valid(question) {
+  return question && question.user && question.content;
+}
+
+app.get('/api/questions', (req, res) => {
+  res.json(questions);
 });
 
-app.listen(app.get('port'), function() {
+app.get('/api/questions/:questionId', (req, res) => {
+  const questionId = req.params.questionId;
+  const questionToFind = questions.find(question => question.id === questionId);
+  if (!questionToFind) {
+    res.sendStatus(404);
+  } else {
+    res.json(questionToFind);
+  }
+});
+
+app.post('/api/questions', (req, res) => {
+  const question = req.body;
+  // if the request is valid
+  if (valid(question)) {
+    // create a new question object with an ID
+    const questionWithId = {
+      id: uuid(),
+      user: question.user,
+      content: question.content
+    };
+    // push it to the "datastore"
+    questions.push(questionWithId);
+    // send the new object back
+    res.json(questionWithId);
+  } else {
+    // if the request is invalid, send a 400
+    res.sendStatus(400);
+  }
+});
+
+app.put('/api/questions/:questionId', (req, res) => {
+  const questionId = req.params.questionId;
+  const newQuestion = req.body;
+  const questionToFind = questions.find(question => question.id === questionId);
+  if (!questionToFind) {
+    res.sendStatus(404);
+  } else if (!valid(newQuestion)) {
+    res.sendStatus(400);
+  } else {
+    // mutate `questionToFind`
+    questionToFind.user = newQuestion.user;
+    questionToFind.content = newQuestion.content;
+    res.json(questionToFind);
+  }
+});
+
+app.delete('/api/questions/:questionId', (req, res) => {
+  const questionId = req.params.questionId;
+  const questionToFind = questions.find(question => question.id === questionId);
+  if (!questionId) {
+    res.sendStatus(400);
+  } else if (!questionToFind) {
+    res.sendStatus(404);
+  } else {
+    const indexToRemove = questions.findIndex(question => question.id === questionId);
+    questions.splice(indexToRemove, 1);
+    res.send();
+  }
+})
+
+app.listen(app.get('port'), () => {
   console.log('Node app is running on port', app.get('port'));
 });
